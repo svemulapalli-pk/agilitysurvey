f38f3f368d22f498db07380b6c270e5b0f117de7
Charwitha Buggaveeti <bcharwitha@prokarma.com>
2020-04-07 20:38:41 +0530

    Committing assessment page changes

--- src/pages/AssessmentPage.js
+++ src/pages/AssessmentPage.js
@@ -2,6 +2,16 @@ import React from "react";
 
 export const AssessmentPage = () => {
   return (
-    <div>Welcome to the SAFe DevOps Assessment</div>
+    <div className="assessment-page">
+      <div className="page-heading">
+        <h3>Welcome to the SAFe DevOps Assessment</h3>
+      </div>
+      <div className="page-content">
+        <form name="assessment-form">
+          <div className="assess-form__steps"></div>
+          <div className="assess-form__form-content"></div>
+        </form>
+      </div>
+    </div>
   );
 };
